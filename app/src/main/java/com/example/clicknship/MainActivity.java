0f8cfe22fea78170dbafd4ae7d0f424b726826f6
low yi liang <yi_liang88@hotmail.com>
2023-09-24 00:44:24 +0800

    implemented api call for login

--- app/src/main/java/com/example/clicknship/MainActivity.java
+++ app/src/main/java/com/example/clicknship/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.clicknship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +15,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,17 +36,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView Password = (TextView) findViewById(R.id.password);
-        ImageView ShopBtn = (ImageView) findViewById(R.id.ShopBtn);
-        Button loginBtn = (Button) findViewById(R.id.loginbtn);
+        AccountManager am = AccountManager.get(this);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        Bundle options = new Bundle();
+
+        TextView username =  findViewById(R.id.username);
+        TextView Password =  findViewById(R.id.password);
+        ImageView ShopBtn =  findViewById(R.id.ShopBtn);
+        Button loginBtn =  findViewById(R.id.loginbtn);
 
         //ShopBtn Login
         ShopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Toast.makeText(MainActivity.this, "login successful", Toast.LENGTH_SHORT).show();
+
+                Account[] accounts = am.getAccountsByType("com.google");
+
+//                am.getAuthToken(
+//                        account,                     // Account retrieved using getAccountsByType()
+//                        "Manage your tasks",            // Auth scope
+//                        options,                        // Authenticator-specific options
+//                        this,                           // Your activity
+//                        new OnTokenAcquired(),          // Callback called when a token is successfully acquired
+//                        new Handler(new OnError()));    // Callback called if an error occurs
 
             }
         });
@@ -37,11 +68,87 @@ public class MainActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener () {
             @Override
             public void onClick(View view) {
-                if (username.getText().toString().equals("HAHA")) {
-                    Toast.makeText(MainActivity.this, "login successful", Toast.LENGTH_SHORT).show();
-                }
+                //sha256 hash for password
+                getSha256Hash(Password.getText().toString());
+                //Need to append password hash to the end of URL then get the respond
+                String url = "https://jsonplaceholder.typicode.com/todos/1";
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                try {
+                                    //int userID = response.getInt("userId");
+                                    String Result = response.getString("title");
+
+                                    if (Result.equals("OK")) {
+                                        Toast.makeText(MainActivity.this, "login successful", Toast.LENGTH_SHORT).show();
+
+                                        final Account account = new Account(username.getText().toString(), "login");
+                                        am.addAccountExplicitly(account, Password.getText().toString(), null);
+                                    }
+                                } catch (JSONException e) {
+                                    Toast.makeText(MainActivity.this, "error:" + e, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this, "login unsuccessful", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                queue.add(jsonObjectRequest);
+
             }
         });
 
+    }
+
+    public static String getSha256Hash(String password) {
+        try {
+            MessageDigest digest = null;
+            try {
+                digest = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e1) {
+                e1.printStackTrace();
+            }
+            digest.reset();
+            return bin2hex(digest.digest(password.getBytes()));
+        } catch (Exception ignored) {
+            return null;
+        }
+    }
+
+    private static String bin2hex(byte[] data) {
+        StringBuilder hex = new StringBuilder(data.length * 2);
+        for (byte b : data)
+            hex.append(String.format("%02x", b & 0xFF));
+        return hex.toString();
+    }
+
+    private class OnTokenAcquired implements AccountManagerCallback<Bundle> {
+        @Override
+        public void run(AccountManagerFuture<Bundle> result) {
+            // Get the result of the operation from the AccountManagerFuture.
+            Bundle bundle = null;
+            try {
+                bundle = result.getResult();
+
+            } catch (AuthenticatorException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (OperationCanceledException e) {
+                throw new RuntimeException(e);
+            }
+
+            // The token is a named value in the bundle. The name of the value
+            // is stored in the constant AccountManager.KEY_AUTHTOKEN.
+            String token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+
+        }
     }
 }
