b1c1570e136bc076245edc800049542798bd0a91
xinenhoe <xinen95@hotmail.com>
2023-10-26 18:46:23 +0800

    Xin En
    display catalogue list of items and removal of webview

--- app/src/main/java/com/example/clicknship/Catalog.java
+++ app/src/main/java/com/example/clicknship/Catalog.java
@@ -4,17 +4,35 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Catalog extends AppCompatActivity {
 
     private WebView Catalog;
+
+    private ListView listView;
+    List<String> catalogList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +42,18 @@ public class Catalog extends AppCompatActivity {
         Button searchBtn = findViewById(R.id.searchB);
         TextView item =  findViewById(R.id.search);
 
-        Catalog = (WebView) findViewById(R.id.webview);
+        //Catalog = (WebView) findViewById(R.id.webview);
         final String searchURL = "https://www.amazon.com/s/?field-keywords=";
         // Here is I make transparent background for WebView
         Catalog.setBackgroundColor(0x00000000);
+
+        // Display catalogue products
+        catalogList = new ArrayList<>();
+        listView = findViewById(R.id.catalogListView);
+
+        getCatalogData getCatData = new getCatalogData();
+        getCatData.execute();
+        // End
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -142,6 +168,68 @@ public class Catalog extends AppCompatActivity {
             Catalog.goBack();
         } else {
             super.onBackPressed();
+        }
+    }
+
+    public class getCatalogData extends AsyncTask<String, String, String> {
+        String result = "";
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection urlConnection = null;
+
+            try {
+                System.out.println("Start of background execution.");
+                URL url = new URL("http://10.0.2.2:8770/api/catalogueService/catalogue/catalogue/");
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                // Check if connection is successful
+                int code = urlConnection.getResponseCode();
+                if (code !=  200) {
+                    throw new IOException("Invalid response from server: " + code);
+                }
+
+                BufferedReader buffReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder strBuilder = new StringBuilder();
+
+                String line = null;
+                while ((line = buffReader.readLine()) != null) {
+                    Log.i("JSON String: ", line);
+                    strBuilder.append(line + "\n");
+                }
+                result = strBuilder.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                // Disconnect all connections
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            System.out.println("End of background execution.");
+            return null;
+        }
+
+        protected void onPostExecute(Void v) {
+            // Parse JSON data to display in ListView
+            try {
+                System.out.println("Start of post execution.");
+                JSONArray jsonArray = new JSONArray(result);
+                for(int i=0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObj = jsonArray.getJSONObject(i);
+
+                    String name = jsonObj.getString("name");
+                    String description = jsonObj.getString("description");
+                    String price = jsonObj.getString("price");
+
+                    catalogList.add(name);
+                    catalogList.add(description);
+                    catalogList.add(price);
+                }
+                System.out.println("End of post execution.");
+            } catch (JSONException e) {
+                Log.e("JSONException", "Error: " + e.toString());
+            }
         }
     }
 }
