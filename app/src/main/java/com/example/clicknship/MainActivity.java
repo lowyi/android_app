2ee252ed5e1b667e23bd3f4b50aca0a0ac8365c5
low yi liang <yi_liang88@hotmail.com>
2023-10-31 23:09:12 +0800

    implement url checking
    Signed-off-by: low yi liang <yi_liang88@hotmail.com>

--- app/src/main/java/com/example/clicknship/MainActivity.java
+++ app/src/main/java/com/example/clicknship/MainActivity.java
@@ -76,72 +76,120 @@ public class MainActivity extends AppCompatActivity {
                 //String url = "http://34.30.227.181:4200/api/authenticationService/oauth/token";
                 String url = "http://192.168.1.71:8770/api/authenticationService/oauth/token";
 
-                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>()
-                        {
-                            @Override
-                            public void onResponse(String response) {
-                                // response
-                                String jsonStr = response;
-                                try {
-                                    Toast.makeText(MainActivity.this, "login successful", Toast.LENGTH_SHORT).show();
+                if (!url.startsWith("file://")) {
+                    if (!url.startsWith("javascript://")) {
+                        if (!url.startsWith("content://")) {
 
-                                    JSONObject obj = new JSONObject(jsonStr);
+                            StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                                    new Response.Listener<String>()
+                                    {
+                                        @Override
+                                        public void onResponse(String response) {
+                                            // response
+                                            String jsonStr = response;
+                                            try {
+                                                Toast.makeText(MainActivity.this, "login successful", Toast.LENGTH_SHORT).show();
 
-                                    pre.setAccessToken(MainActivity.this,obj.getString("token"), obj.getString("refreshToken"));
+                                                JSONObject obj = new JSONObject(jsonStr);
 
-                                    Intent intent = new Intent(MainActivity.this,Catalog.class);
-                                    intent.putExtra("expire", obj.getString("expiresIn"));
-                                    startActivity(intent);
+                                                pre.setAccessToken(MainActivity.this,obj.getString("token"), obj.getString("refreshToken"));
 
-                                } catch (JSONException e) {
-                                    throw new RuntimeException(e);
+                                                Intent intent = new Intent(MainActivity.this,Catalog.class);
+                                                intent.putExtra("expire", obj.getString("expiresIn"));
+                                                startActivity(intent);
+
+                                            } catch (JSONException e) {
+                                                throw new RuntimeException(e);
+                                            }
+                                        }
+                                    },
+                                    new Response.ErrorListener()
+                                    {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
+                                            Toast.makeText(MainActivity.this, "login unsuccessful" + error, Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }
+                            ) {
+                                @Override
+                                public String getBodyContentType() {
+                                    return "application/x-www-form-urlencoded; charset=UTF-8";
                                 }
-                            }
-                        },
-                        new Response.ErrorListener()
-                        {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(MainActivity.this, "login unsuccessful" + error, Toast.LENGTH_SHORT).show();
 
-                            }
+                                @Override
+                                protected Map<String, String> getParams() throws AuthFailureError {
+                                    Map<String, String> params = new HashMap<String, String>();
+                                    params.put("grant_type", "password");
+                                    params.put("client_id", CLIENT_ID);
+                                    params.put("client_secret", CLIENT_SECRET);
+                                    params.put("username", username.getText().toString().trim());
+                                    try {
+                                        byte[] dataP = Password.getText().toString().trim().getBytes("UTF-8");
+                                        byte[] dataPK = "go2shopSecretKey".getBytes("UTF-8");
+
+                                        params.put("password", encryptStrAndToBase64(dataPK, dataP));
+                                    } catch (Exception e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                    try {
+                                        byte[] dataOTP = otp.getText().toString().trim().getBytes("UTF-8");
+                                        byte[] dataOTPK = "go2shopSecretKey".getBytes("UTF-8");
+
+                                        params.put("otp", encryptStrAndToBase64(dataOTPK, dataOTP));
+                                    } catch (Exception e) {
+                                        throw new RuntimeException(e);
+                                    }
+
+                                    return params;
+                                }
+                            };
+
+                            queue.add(postRequest);
+
+                        }else{
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setTitle("Illegal Website Access")
+                                    .setMessage("You Have Access An Illegal website To protect your data safety, you are not allowed to use this app.")
+                                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                                            finishAffinity();
+                                        }
+                                    })
+                                    .setCancelable(false)
+                                    .show();
                         }
-                ) {
-                    @Override
-                    public String getBodyContentType() {
-                        return "application/x-www-form-urlencoded; charset=UTF-8";
+                    }else{
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Illegal Website Access")
+                                .setMessage("You Have Access An Illegal website To protect your data safety, you are not allowed to use this app.")
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                        finishAffinity();
+                                    }
+                                })
+                                .setCancelable(false)
+                                .show();
                     }
+                }else{
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Illegal Website Access")
+                            .setMessage("You Have Access An Illegal website To protect your data safety, you are not allowed to use this app.")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
 
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> params = new HashMap<String, String>();
-                        params.put("grant_type", "password");
-                        params.put("client_id", CLIENT_ID);
-                        params.put("client_secret", CLIENT_SECRET);
-                        params.put("username", username.getText().toString().trim());
-                        try {
-                            byte[] dataP = Password.getText().toString().trim().getBytes("UTF-8");
-                            byte[] dataPK = "go2shopSecretKey".getBytes("UTF-8");
+                                    finishAffinity();
+                                }
+                            })
+                            .setCancelable(false)
+                            .show();
+                }
 
-                            params.put("password", encryptStrAndToBase64(dataPK, dataP));
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                        try {
-                            byte[] dataOTP = otp.getText().toString().trim().getBytes("UTF-8");
-                            byte[] dataOTPK = "go2shopSecretKey".getBytes("UTF-8");
-
-                            params.put("otp", encryptStrAndToBase64(dataOTPK, dataOTP));
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-
-                        return params;
-                    }
-                };
-
-                queue.add(postRequest);
             }
         });
 
